48309238ee3d3844602e673ec1634421efd05bc0
mmroczny <35624523+mmroczny@users.noreply.github.com>
2021-06-27 14:10:51 +0200

    Update l_baselinker.py

--- l_baselinker.py
+++ l_baselinker.py
@@ -3,7 +3,7 @@ import json
 
 
 
-API_KEY = "1799-15079-IKXLUTWB3WTKCPQ567D91TSP1S66YXF2F5S9Q0N0XFV4NL1ZXXXXXXXXXXXXXXXX"
+API_KEY = "1234-12345-IKXLUTWB3WTKCPQ567D91TSP1S66YXF2F5S9Q0N0XFV4NL1ZXXXXXXXXXXXXXXXX"
 def baselinker_request(method, params = {}):
   return requests.post("https://api.baselinker.com/connector.php", data={
     "token": API_KEY,
